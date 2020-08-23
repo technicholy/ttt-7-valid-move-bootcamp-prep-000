bdfc65962e20218cdec886b38130bfaf224c5b7b
Clayton Pierce <bamer78@gmail.com>
2020-08-23 23:17:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,8 @@
 def valid_move?(board, index)
   if position_taken?(board, index)
     return false
+  elsif board[index] < 0 || board[index] > 8
+    return false    
   else
     return true
   end
