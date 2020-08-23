d636392f4c2d6fcbdfe2da868e41f04cd2fdf490
Clayton Pierce <bamer78@gmail.com>
2020-08-23 23:13:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  (board[index] ==  "X" || board[index] == "O" || board[index] < 0 || board[index] > 8) ?
+  (position_taken(board_index)) ?
     false : true
 end
 
